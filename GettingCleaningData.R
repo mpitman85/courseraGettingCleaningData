f48a2cae2539b226ca18fe143954962b40277e2b
mpitman85 <megan.pitman@gmail.com>
2017-03-29 16:30:24 -0400

    downloaded documentation file for XML R package

--- GettingCleaningData.R
+++ GettingCleaningData.R
@@ -4,3 +4,6 @@ setwd("/Users/meganpitman/Dropbox/Projects/Classes/Coursera/DataScienceSpecializ
 download.file(
         "https://www.stat.berkeley.edu/~statcur/Workshop2/Presentations/XML.pdf",
         destfile="XPath.pdf", method="curl")
+## Download documentation on XML package
+download.file("http://www.omegahat.net/RSXML/shortIntro.pdf", 
+              destfile="XMLpackage.pdf", method="curl")
