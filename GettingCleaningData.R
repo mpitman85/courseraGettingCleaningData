8df2ffb77c7b7a921524f5ea649cfe0a9eca7b52
mpitman85 <megan.pitman@gmail.com>
2017-03-29 16:09:56 -0400

    downloaded pdf file lecture on XPath

--- GettingCleaningData.R
+++ GettingCleaningData.R
@@ -1,1 +1,6 @@
 setwd("/Users/meganpitman/Dropbox/Projects/Classes/Coursera/DataScienceSpecialization/GettingCleaningData/courseraGettingCleaningData")
+
+## Download lecture on X Path language and extracting data from xml
+download.file(
+        "https://www.stat.berkeley.edu/~statcur/Workshop2/Presentations/XML.pdf",
+        destfile="XPath.pdf", method="curl")
